33f0a83817c03667fea726c813a481e9fdb8aee5
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-05-14 15:49:13 +1100

    offset

--- task5.cpp
+++ task5.cpp
@@ -11,13 +11,40 @@ void initialising_game_sector_number(bool is_game_sector_number[]) {
 
 }
 
+void offset_of_the_current_sector_on_the_reel(int& offset, bool is_game_sector_number[]) {
 
+	
+
+}
 
 int main() {
 
 	bool is_game_sector_number[5];
 
 	initialising_game_sector_number(is_game_sector_number);
+
+	int offset;
+
+	std::cout << "Please, enter offset: ";
+	std::cin >> offset;
+
+	is_game_sector_number[0] = true;
+
+	if (offset > 5) {
+
+		offset = offset % 5;
+
+	}
+	
+	while (!is_game_sector_number[offset - 1] == false) {
+
+		++offset;
+
+	}
+
+	is_game_sector_number[offset - 1] = true;
+
+	std::cout << "offset = " << offset << "\n";
 
 
 	for (int i = 0; i < 5; ++i) {
