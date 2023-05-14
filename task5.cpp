639ee82b59ce595992377e156eb2770ca003cfa2
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-05-14 20:05:17 +1100

    read in function

--- task5.cpp
+++ task5.cpp
@@ -35,16 +35,8 @@ void offset_of_the_current_sector_on_the_reel(int& offset, bool is_game_sector_n
 
 }
 
-void win(std::ifstream f_question) {
+void win(bool is_game_sector_number[], int& offset, std::ifstream& f_question, std::ifstream& f_answer) {
 
-
-
-}
-
-int main() {
-
-	bool is_game_sector_number[5];
-	int offset;
 	int point_connoisseurs = 0;
 	int point_viewers = 0;
 
@@ -52,6 +44,81 @@ int main() {
 
 	std::string buffer_question;
 	std::string buffer_answers;
+
+	while ((point_connoisseurs != 2) || (point_viewers != 2)) {
+
+		offset_of_the_current_sector_on_the_reel(offset, is_game_sector_number);
+
+
+		for (int i = 0; i < offset; ++i) {
+
+			getline(f_question, buffer_question);
+			f_answer >> buffer_answers;
+
+		}
+
+
+
+
+		std::cout << "Attention question: " << buffer_question << std::endl;
+		std::cout << "answers: " << buffer_answers << std::endl;
+
+
+
+
+
+
+		std::cout << "Connoisseurs, please enter the answer: ";
+		std::cin >> the_answer_of_connoisseurs;
+
+		if (buffer_answers == the_answer_of_connoisseurs) {
+
+
+			std::cout << "You answered correctly!" << std::endl;
+			std::cout << "Expert team wins 1 point" << std::endl;
+
+			++point_connoisseurs;
+
+		}
+		else {
+
+
+			std::cout << "You answered incorrectly!\n";
+			std::cout << "Correct answer: " << buffer_answers << std::endl;
+			std::cout << "1 point goes to the viewer team" << std::endl;
+
+			++point_viewers;
+		}
+
+		std::cout << "Game Score: " << point_connoisseurs << ":" << point_viewers << std::endl;
+
+		if (point_connoisseurs > point_viewers) {
+
+			std::cout << "The expert team wins\n";
+
+		}
+		else if (point_connoisseurs < point_viewers) {
+
+			std::cout << "Viewer team wins\n";
+
+		}
+		else {
+
+			std::cout << "Draw in the game\n";
+
+		}
+
+
+
+	}
+
+}
+
+int main() {
+
+	bool is_game_sector_number[5];
+	int offset;
+	
 
 	std::ifstream f_question;
 	std::ifstream f_answer;
@@ -92,74 +159,9 @@ int main() {
 
 	}
 
-	while ((point_connoisseurs != 2) || (point_viewers != 2)) {
+	
 
-		offset_of_the_current_sector_on_the_reel(offset, is_game_sector_number);			
-
-
-		for (int i = 0; i < offset; ++i) {
-
-			getline(f_question, buffer_question);
-			f_answer >> buffer_answers;
-
-		}
-
-
-
-		
-		std::cout << "Attention question: " << buffer_question << std::endl;
-		std::cout << "answers: " << buffer_answers << std::endl;
-
-
-
-
-
-
-		std::cout << "Connoisseurs, please enter the answer: ";
-		std::cin >> the_answer_of_connoisseurs;
-
-		if (buffer_answers == the_answer_of_connoisseurs) {
-						
-
-			std::cout << "You answered correctly!" << std::endl;
-			std::cout << "Expert team wins 1 point" << std::endl;
-
-			++point_connoisseurs;
-
-		}
-		else {
-
-			
-			std::cout << "You answered incorrectly!\n";
-			std::cout << "Correct answer: " << buffer_answers << std::endl;
-			std::cout << "1 point goes to the viewer team" << std::endl;
-
-			++point_viewers;
-		}
-
-		std::cout << "Game Score: " << point_connoisseurs << ":" << point_viewers << std::endl;
-
-		if (point_connoisseurs > point_viewers) {
-
-			std::cout << "The expert team wins\n";
-
-		}
-		else if (point_connoisseurs < point_viewers) {
-
-			std::cout << "Viewer team wins\n";
-
-		}
-		else {
-
-			std::cout << "Draw in the game\n";
-
-		}
-
-
-
-	}
-
-
+	win( is_game_sector_number, offset, f_question, f_answer);
 
 
 
